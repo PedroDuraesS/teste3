944c7c7526b225b6c7aded35986f8368a9ef2386
BernardooVale <103006006+BernardooVale@users.noreply.github.com>
2022-08-19 08:58:35 -0300

    Add files via upload

---
+++ Assets/códigos/FirstPersonCamera.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FirstPersonCamera : MonoBehaviour
+{
+    public Transform characterBody;
+    public Transform characterHead;
+
+    float rotationX = 0;
+    float rotationY = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private void LateUpdate()
+    {
+        transform.position = characterHead.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float verticalDelta = Input.GetAxisRaw("Mouse Y");
+        float horizontalDelta = Input.GetAxisRaw("Mouse X");
+
+        rotationX += horizontalDelta;
+        rotationY += verticalDelta;
+
+        transform.localEulerAngles = new Vector3(-rotationY, rotationX, 0);
+
+    }
+}
